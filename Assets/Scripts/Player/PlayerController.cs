4601e5f29247f5dbd856d6222afc7f29171466cf
Fenantren <p.paszko93@gmail.com>
2025-11-17 19:22:46 +0000

    Player controller script and Player Input map updated
    - Send Messages switched to CallbackContext
    - Slam mechanic added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,13 +7,17 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     BoxCollider boxCollider;
+    
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float dashSpeed = 10f;
     [SerializeField] float turnSpeed = 360f;
     [SerializeField] float jumpForce = 10f;
+    [SerializeField] float jumpAcceleration = 5f;
     [SerializeField] float currentSpeed;
-    
-    
+    [SerializeField] float slamForce = -20f;
+
+    [SerializeField] bool canSlam = false;
+    [SerializeField] bool isGrounded;
     private Vector3 input;
 
     PlayerInputs playerInputs;
@@ -29,27 +33,22 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
+
+        IsGrounded();
         
         
-        GatherInput();
         
-        Dash();
 
     }
 
 
     void FixedUpdate()
     {
-        Move();
+        MovePlayer();
         Look();
 
-        Jump();
     }
 
-    void GatherInput()
-    {
-        input = new Vector3(playerInputs.move.x, 0, playerInputs.move.y);
-    }
 
     private void Look()
     {
@@ -62,46 +61,63 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Move()
+    void MovePlayer()
     {
         
         rb.MovePosition(transform.position + (transform.forward * input.magnitude) * currentSpeed * Time.deltaTime);
     }
 
-    void Dash()
+    public void Dash(InputAction.CallbackContext context)
     {
-        if (playerInputs.dash)
+        if (context.started)
         {
             currentSpeed = dashSpeed;
         }
-        else
+        else if (context.canceled)
         {
             currentSpeed = moveSpeed;
         }
     }
 
-    void Jump()
+    public void Jump(InputAction.CallbackContext context)
     {
-        if ( IsGrounded() && playerInputs.jump)
+        if ( isGrounded && context.performed )
         {
-            rb.linearVelocity = Vector3.up * jumpForce;
-           
+            rb.linearVelocity = Vector3.up * jumpForce * jumpAcceleration;
+            canSlam = true; 
+            
         }
 
+        else if( canSlam && context.performed)
+        {
+            rb.linearVelocity = -Vector3.up * slamForce;
+            canSlam = false;
+        }
+        
+        if ( context.canceled && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y * 0.75f, rb.linearVelocity.z);
+        }
     }
 
-    bool IsGrounded()
+    void IsGrounded()
     {
         float radius = 0.2f;
         
         if( Physics.CheckSphere(groundCheck.transform.position, radius, groundLayer))
         {
-            return true;
+            isGrounded = true;
+            
         }
         else
         {
-            return false;
+            isGrounded = false;
+            
         }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        input = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
     }
 
     private void OnDrawGizmos()
@@ -109,4 +125,6 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(groundCheck.transform.position, 0.2f);
     }
+
+    
 }
