f56239e00a07d4f53e1567af583a5d330510b810
Fenantren <p.paszko93@gmail.com>
2025-11-30 22:54:31 +0000

    Tutorial finished
    -fully implemented tutorial
    - created simpleton for background music

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -7,7 +7,19 @@ public class TutorialScript : MonoBehaviour
     [SerializeField] string[] tutorialTextArray;
     [SerializeField] TMP_Text tutorialText;
     [SerializeField] int currentIndex = 0;
+    [SerializeField] GameObject enterText;
+    [SerializeField] GameObject waveTower;
 
+
+    private void Update()
+    {
+        if(currentIndex == 7 && !waveTower)
+        {
+            
+            currentIndex = 8;
+            tutorialText.text = tutorialTextArray[currentIndex];    
+        }
+    }
     public void Tutorial(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -18,10 +30,17 @@ public class TutorialScript : MonoBehaviour
 
     public void NextTextLine()
     {
-        if (currentIndex < tutorialTextArray.Length)
+        if (currentIndex < 7)
         {
             currentIndex++;
             tutorialText.text = tutorialTextArray[currentIndex];
+        }
+        
+        
+        if (currentIndex == 7)
+        {
+            waveTower.SetActive(true);
+            enterText.SetActive(false);
         }
     }
 }
